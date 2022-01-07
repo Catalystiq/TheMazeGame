c403d7f98453390d478930186f02bbae0a029170
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2022-01-07 10:01:11 -0700

    fixed movement system for MovingBarrier
    they now work pog

--- MovingBarrier.java
+++ MovingBarrier.java
@@ -1,7 +1,9 @@
 import java.awt.*;
 
 public class MovingBarrier extends Rectangle{
-    int speed = 1;
+   int speed = 5;
+   boolean up = false;
+   boolean down = true;
 
    MovingBarrier(int x, int y, int w, int h){
       super(x, y, w, h);
@@ -13,10 +15,36 @@ public class MovingBarrier extends Rectangle{
    }
 
    public void move(){
-       if(y >= 160){
-            y -= speed;
-       }else if(y <= 366){
-            y += speed;
-       }
+      //System.out.println(y);
+      if(down){
+         if(y < 366){
+            y += speed;  
+         }
+         if(y >= 366){
+            down = false;
+            up = true;
+         }
+      }
+      if(up){
+         if(y > 160){
+            y -= speed;  
+         }
+         if(y <= 160){
+            down = true;
+            up = false;
+         }
+      }
+
+
+      //   if(y < 366){
+      //       y -= speed;
+      //  }
+      //   if(y > 160){
+      //       y += speed;
+      //  }else if(y <= 160){
+      //     y -= 5;
+      //  } if(y >= 366){
+      //    y -= 20;
+      // }
    }
 }
