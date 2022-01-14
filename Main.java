68bd95d6bb9752ef8b9c69026bf88bc33f117e50
Catalystiq <jamesnel.exe@gmail.com>
2022-01-14 08:20:04 -0700

    ad
    fasdf

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ import javax.swing.*;
 
 public class Main extends JFrame{
     public static void main(String[] args){
-        ImageIcon pong = new ImageIcon("./img/pong.png");
+        ImageIcon splash = new ImageIcon("./img/MazeSplashScreen.png");
         JOptionPane.showMessageDialog(null,
                " Welcome to The Maze Game \n This is my final project for Computer Programming I \n You are in a maze and avoid obstacles to reach the doors. \n There are 3 levels that more difficult as you go on. \n Control your character with WASD \n Click OK to begin! \n Have Fun and Enjoy!", "TheMazeGame",
-               JOptionPane.INFORMATION_MESSAGE, pong);
+               JOptionPane.INFORMATION_MESSAGE, splash);
 
         GameFrame frame = new GameFrame();
     }
